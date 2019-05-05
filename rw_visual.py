de9221eadb478c4dcdb7627b23ef75297390c2d1
malinroot <kurchevskijpavel@gmail.com>
2019-05-05 23:53:35 +0300

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -8,6 +8,9 @@ while True:
     rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # 
+    plt.figure(figsize=(10, 6))
+
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
                 edgecolors='none', s=1)
