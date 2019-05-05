17b17bedd61ce20b9f72a1e7256f937dbad48296
malinroot <kurchevskijpavel@gmail.com>
2019-05-05 23:33:40 +0300

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Construction of random walk and drawing points on the chart. 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# New walks are built as long as the program remains active.
+while True:
+    # Construction of random walk and drawing points on the chart. 
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolors='none', s=1)
+    
+    # Select the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+                s=100)
+    
+    # Removing axles.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make anther walk? (y/n): ")
+    if keep_running == 'n':
+        break
+
