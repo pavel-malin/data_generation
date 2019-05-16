7d2dfdfde32a6195bb31f76de2ac066eaa727786
malinroot <kurchevskijpavel@gmail.com>
2019-05-17 00:17:36 +0300

    Add files via upload

--- country_codes.py
+++ country_codes.py
@@ -4,7 +4,11 @@ def get_country_code(country_name):
     """Returns for a given country its Pygal code,
      consisting of 2 letters."""
     for code, name in COUNTRIES.items():
-        if name == country_name:
+        if country_name == 'Yemen, Rep':
+            return 'ye'
+        elif name == country_name:
             return code
     # If no country is found, return None.
     return None
+
+print(get_country_code('United Arab Emirates'))
